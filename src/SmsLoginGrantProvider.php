cff7e7ca46c84697ef2ff9fa43ea30d706772257
XuTongle <xutongle@gmail.com>
2018-10-11 10:21:11 +0800

    c

--- src/SmsLoginGrantProvider.php
+++ src/SmsLoginGrantProvider.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * @link http://www.tintsoft.com/
- * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
- * @license http://www.tintsoft.com/license/
- */
-
 namespace XuTL\Passport\Sms;
 
 use Laravel\Passport\Bridge\RefreshTokenRepository;
--- src/SmsRequestGrant.php
+++ src/SmsRequestGrant.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * @link http://www.tintsoft.com/
- * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
- * @license http://www.tintsoft.com/license/
- */
 
 namespace XuTL\Passport\Sms;
 
